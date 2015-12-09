94e00e68956f6966b2dfd3f0d28c48f78445f5c1
Laura Sophie <info@soffelweb.de>
2015-12-09 19:41:09 +0100

    Math Added

---
+++ Wie Denke Ich?/Simplemath.swift
@@ -0,0 +1,18 @@
+//
+//  Simplemath.swift
+//  Wie Denke Ich?
+//
+//  Created by Laura Sophie on 09.12.15.
+//  Copyright © 2015 Laura Sophie Bertsch. All rights reserved.
+//
+
+import Foundation
+
+class Operations {
+    func add() {
+        print(String("Führe Addition aus"))
+    }
+    func sub(){
+        print(String("Führe Substraktion aus"))
+    }
+}
