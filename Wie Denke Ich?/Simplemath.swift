8a71d378508aed63544555d0aa111f9a5562ecf5
Laura Sophie <info@soffelweb.de>
2015-12-11 22:23:28 +0100

    Math v.01

--- Wie Denke Ich?/Simplemath.swift
+++ Wie Denke Ich?/Simplemath.swift
@@ -6,13 +6,23 @@
 //  Copyright © 2015 Laura Sophie Bertsch. All rights reserved.
 //
 
-import Foundation
+//import Foundation
 
-class Operations {
-    func add() {
+protocol Operations {
+    func add(a:Int, b:Int) ->Int {
+        print(String("Hole Zahl 1"))
+        print(String("Hole Zahl 2"))
         print(String("Führe Addition aus"))
+        let result:Int = a + b
+        print(String("Gebe Ergebins aus"))
+        return result
     }
-    func sub(){
+    func sub(a:Int, b:Int) ->Int {
+        print(String("Hole Zahl 1"))
+        print(String("Hole Zahl 2"))
         print(String("Führe Substraktion aus"))
+        let result:Int = a + b
+        print(String("Gebe Ergebins aus"))
+        return result
     }
 }
--- Wie Denke Ich?/ViewController.swift
+++ Wie Denke Ich?/ViewController.swift
@@ -8,18 +8,6 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, Operations{
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-}
 
